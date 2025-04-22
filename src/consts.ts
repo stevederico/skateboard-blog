8fcab6eb596637fee297dab2d6f396829f6a8b5f
Steve Derico <steve@bixbyapps.com>
2025-04-22 11:01:15 -0700

    first

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'My Blog';
+export const SITE_DESCRIPTION = 'A blog about everything';
+export const SITE_COMPANY = 'Company Inc';
